3ecf51524b03e82291a6f5bc2f47f47438182ffd
Koorosh cPanel <kooroshkz@yahoo.com>
2024-10-19 18:51:02 +0330

    update(Django): Added chatbot app and updated project structure

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,23 @@
-import imp
-import os
 import sys
+import os
+from django.core.wsgi import get_wsgi_application
 
+# Set the project base directory
+project_path = '/home/kooroshkz/softdev.kooroshkz.com/guido_chatbot'
+sys.path.insert(0, project_path)
 
-sys.path.insert(0, os.path.dirname(__file__))
+# Set the path to the virtual environment site-packages
+venv_path = '/home/kooroshkz/virtualenv/softdev.kooroshkz.com/3.11/lib/python3.11/site-packages'
+sys.path.insert(1, venv_path)
 
-wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
-application = wsgi.application
+# Set the Django settings module environment variable
+os.environ['DJANGO_SETTINGS_MODULE'] = 'guido_chatbot.settings'
+
+# Activate the virtual environment
+activate_env = '/home/kooroshkz/virtualenv/softdev.kooroshkz.com/3.11/bin/activate_this.py'
+with open(activate_env) as file_:
+    exec(file_.read(), {'__file__': activate_env})
+
+# Create the WSGI application
+application = get_wsgi_application()
+
